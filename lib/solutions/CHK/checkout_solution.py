49aeeec85b1d613ad809719aea128e8fd097844f
sbx_user1061 <sbx_user1061@ip-10-12-212-160.eu-west-2.compute.internal>
2018-06-27 07:21:58 +0000

    Wed Jun 27 07:21:58 UTC 2018

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,5 +1,16 @@
 import re
 
+# Our price table and offers:
+# +------+-------+------------------------+
+# | Item | Price | Special offers         |
+# +------+-------+------------------------+
+# | A    | 50    | 3A for 130, 5A for 200 |
+# | B    | 30    | 2B for 45              |
+# | C    | 20    |                        |
+# | D    | 15    |                        |
+# | E    | 40    | 2E get one B free      |
+# +------+-------+------------------------+
+
 # skus = unicode string
 # normal = regular unit price
 # product = character representing product
