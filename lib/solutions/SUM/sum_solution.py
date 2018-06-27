b30102d8058b1bd9a666f14b6a1310710972af2f
sbx_user1061 <sbx_user1061@ip-10-11-133-102.eu-west-2.compute.internal>
2018-06-27 05:20:59 +0000

    Wed Jun 27 05:20:59 UTC 2018

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -3,4 +3,4 @@ def compute(x, y):
  # param[0] = a positive integer between 0-100
  # param[1] = a positive integer between 0-100
  # @return = an Integer representing the sum of the two numbers
- raise NotImplementedError()
+ return x + y
