71dd4ee5eb6a8d52e1710005d8dc8020e92facee
sbx_user1061 <sbx_user1061@ip-10-12-212-160.eu-west-2.compute.internal>
2018-06-27 06:35:23 +0000

    Wed Jun 27 06:35:23 UTC 2018

--- test/solution_tests/TST/test_one.py
+++ test/solution_tests/TST/test_one.py
@@ -1,10 +1,16 @@
 import unittest
 
 from solutions.TST import one
+from solutions.CHK import checkout_solution
 
 class TestSum(unittest.TestCase):
     def test_sum(self):
         self.assertEqual(one.get(), 1)
+
+
+class TestCHK(unittest.TestCase):
+    def test_chk_multipleA(self):
+        self.assertEqual(checkout_solution.checkout("AAA"), 130)
 
 if __name__ == '__main__':
     unittest.main()
