7217c391da7699b39e6aee6e83769a389dcc6ecd
sbx_user1061 <sbx_user1061@ip-10-12-212-160.eu-west-2.compute.internal>
2018-06-27 07:02:23 +0000

    Wed Jun 27 07:02:23 UTC 2018

--- test/solution_tests/TST/test_one.py
+++ test/solution_tests/TST/test_one.py
@@ -12,11 +12,26 @@ class TestCHK(unittest.TestCase):
     def test_chk_multipleA_1(self):
         self.assertEqual(checkout_solution.checkout("AAA"), 130)
 
-    def test_chk_multipleA_2(self):
+    def test_chk_multipleAB1(self):
         self.assertEqual(checkout_solution.checkout("ABABA"), 175)
 
-    #def test_chk_multipleA_3(self):
-    #    self.assertEqual(checkout_solution.checkout("ABBBBAA"), 210)
+    def test_chk_multipleAB2(self):
+        self.assertEqual(checkout_solution.checkout("ABBBBAA"), 220)
+
+    def test_chk_multipleAB_and_C(self):
+        self.assertEqual(checkout_solution.checkout("ABBBBAACCC"), 220)
+
+    def test_chk_multipleCD(self):
+        self.assertEqual(checkout_solution.checkout("CD"), 220)
+
+    def test_chk_empty_string(self):
+        self.assertEqual(checkout_solution.checkout(""), 220)
+
+    def test_chk_string_invalid_products(self):
+        self.assertEqual(checkout_solution.checkout("EFG"), 220)
+
+    def test_chk_multipleAB_and_C(self):
+        self.assertEqual(checkout_solution.checkout("ABBBBAACCC"), 220)
 
 if __name__ == '__main__':
     unittest.main()
