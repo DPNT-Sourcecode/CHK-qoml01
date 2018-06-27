270a948d463dae0e8cdc580472fa7ed09ad4bac6
sbx_user1061 <sbx_user1061@ip-10-12-212-160.eu-west-2.compute.internal>
2018-06-27 07:27:58 +0000

    Wed Jun 27 07:27:58 UTC 2018

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -14,13 +14,16 @@ import re
 # skus = unicode string
 # normal = regular unit price
 # product = character representing product
-# special = pair (amount, special_offer) where amount is multiple and special offer
+# special = a list of pairs (amount, special_offer) where amount is multiple and special offer
 # @ return = the total price including any special offers
 def get_totals(skus, product, normal, special):
     p_count = skus.count(product)
     special_part = 0
     normal_part = 0
     if special:
+        # sort the special offers by amount
+        sorted_specials = 
+
         special_part = (p_count/special[0]) * special[1]
         normal_part = (p_count % special[0]) * normal
     else:
@@ -36,8 +39,7 @@ def checkout(skus):
     total = 0
     #invalid input = anything not ABCD
     if(re.match('^[ABCD]*$', skus)):
-        total += get_totals(skus, 'A', 50, (5, 200))
-        total += get_totals(skus, 'A', 50, (3, 130))
+        total += get_totals(skus, 'A', 50, [(5, 200), (3,130)])
         total += get_totals(skus, 'B', 30, (2, 45))
         total += get_totals(skus, 'C', 20, None)
         total += get_totals(skus, 'D', 15, None)
