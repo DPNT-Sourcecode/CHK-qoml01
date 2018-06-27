e7e41329820c3a2b5e6047b355fea2b009a639c4
sbx_user1061 <sbx_user1061@ip-10-12-212-160.eu-west-2.compute.internal>
2018-06-27 06:56:23 +0000

    Wed Jun 27 06:56:23 UTC 2018

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,25 +3,23 @@
 # normal = regular unit price
 # product = character representing product
 # special = pair (amount, special_offer) where amount is multiple and special offer
-def totals(skus, product, normal, special):
+# @ return = the total price including any special offers
+def get_totals(skus, product, normal, special):
     p_count = skus.upper().count(product)
-    specials = (p_count/special.first) * special.second
-    normal_a = (p_count % special.first) * normal
-    total = special_a + normal_a
-
-
+    if special:
+        special_part = (p_count/special.first) * special.second
+    normal_part = (p_count % special.first) * normal
+    total = special_part + normal_part
+    return total
 
 # noinspection PyUnusedLocal
 # skus = unicode string
 # @return = an Integer representing the total checkout value of the items
 def checkout(skus):
     total = 0
-    a_count = skus.upper().count('A')
-    special_a = (a_count/3) * 130
-    normal_a = (a_count % 3) * 50
-    total = special_a + normal_a
-
-
-
+    total += get_totals(skus, 'A', 50, (3, 130))
+    total += get_totals(skus, 'B', 30, (2, 45))
+    total += get_totals(skus, 'C', 20, None)
+    total += get_totals(skus, 'D', 15, None)
     return total
 
