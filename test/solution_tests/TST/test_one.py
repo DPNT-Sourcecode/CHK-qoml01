34b9e428c3dedc7cd8059b5a6e30ebc02fbde6c6
sbx_user1061 <sbx_user1061@ip-10-12-212-160.eu-west-2.compute.internal>
2018-06-27 06:32:24 +0000

    Wed Jun 27 06:32:24 UTC 2018

--- test/solution_tests/TST/test_one.py
+++ test/solution_tests/TST/test_one.py
@@ -2,11 +2,9 @@ import unittest
 
 from solutions.TST import one
 
-
 class TestSum(unittest.TestCase):
     def test_sum(self):
         self.assertEqual(one.get(), 1)
-
 
 if __name__ == '__main__':
     unittest.main()
