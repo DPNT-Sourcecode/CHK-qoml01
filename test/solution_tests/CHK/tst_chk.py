82f76c819cd814f7d60b0ad3b2070519a18291b2
sbx_user1061 <sbx_user1061@ip-10-12-139-115.eu-west-2.compute.internal>
2018-06-27 06:20:14 +0000

    Wed Jun 27 06:20:14 UTC 2018

--- test/solution_tests/CHK/tst_chk.py
+++ test/solution_tests/CHK/tst_chk.py
@@ -5,7 +5,7 @@ from solutions.CHK import checkout_solution
 
 class TestCHK(unittest.TestCase):
     def test_chk_multipleA(self):
-        self.assertEqual(chk_solution.checkout("AAA"), 130)
+        self.assertEqual(checkout_solution.checkout("AAA"), 130)
 
 if __name__ == '__main__':
     unittest.main()
