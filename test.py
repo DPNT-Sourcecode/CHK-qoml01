39422fdf7660cce96b8d79ef1b192ed534708bbd
sbx_user1061 <sbx_user1061@ip-10-12-139-115.eu-west-2.compute.internal>
2018-06-27 05:57:46 +0000

    Wed Jun 27 05:57:46 UTC 2018

--- test.py
+++ test.py
@@ -1,6 +1,11 @@
+# coding=utf-8
+
 import sys
 from runner.user_input_action import get_user_input
 
 if __name__ == '__main__':	
 	i = get_user_input(sys.argv[1:])
 	print i
+
+	s = unicode("café", 'utf8')
+	print "Hello %s" % s
