2581040e24fc0452b721b02b2c8de96080f5b9d6
sbx_user1061 <sbx_user1061@ip-10-11-133-102.eu-west-2.compute.internal>
2018-06-27 05:18:40 +0000

    Wed Jun 27 05:18:40 UTC 2018

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,3 +1,6 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def compute(x, y):
-    raise NotImplementedError()
+ # param[0] = a positive integer between 0-100
+ # param[1] = a positive integer between 0-100
+ # @return = an Integer representing the sum of the two numbers
+ raise NotImplementedError()
