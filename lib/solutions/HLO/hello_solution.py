afb300cb440c80998355b454d44f1dbd0b34695c
sbx_user1061 <sbx_user1061@ip-10-12-139-115.eu-west-2.compute.internal>
2018-06-27 06:06:31 +0000

    Wed Jun 27 06:06:31 UTC 2018

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -4,7 +4,7 @@
 # friend_name = unicode string
 #@return = a String containing a message
 def hello(friend_name):
-    friend_name.strip('\"')
-    return ("Hello, %s!" % friend_name)
+    fn = friend_name.strip('"')
+    return ("Hello, %s!" % fn)
 
 
